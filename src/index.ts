64172fc49d1839008a2f5f8630469273a5b3c923
SorryToPerson <1184958246@qq.com>
2023-06-12 17:56:45 +0800

    fix: bugfix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: xulibang
  * @Date: 2023-06-12 11:32:22
  * @LastEditors: xulibang
- * @LastEditTime: 2023-06-12 17:50:31
+ * @LastEditTime: 2023-06-12 17:55:46
  * @FilePath: /slim-utils/src/index.ts
  * @Description:
  */
@@ -16,8 +16,8 @@ export const deepClone = (obj: any) => {
     if (obj instanceof Date) return new Date(obj);
     if (obj instanceof RegExp) return new RegExp(obj);
     if (typeof obj !== "object") return obj;
-    let cloneObj = new obj.constructor();
-    for (let key in obj) {
+    const cloneObj = new obj.constructor();
+    for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
             cloneObj[key] = deepClone(obj[key]);
         }
